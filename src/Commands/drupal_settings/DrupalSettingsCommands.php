ddfcea0532925688a8a537a889c22589f61bac88
Eirik S. Morland <eirik@morland.no>
2017-10-30 08:44:12 +0100

    Delete duplicate command file. Fix up imports and comments.

--- src/Commands/drupal_settings/DrupalSettingsCommands.php
+++ src/Commands/drupal_settings/DrupalSettingsCommands.php
@@ -1,19 +1,11 @@
 <?php
 namespace Drush\Commands\drupal_settings;
 
-use Drupal\Core\DrupalKernel;
 use Drupal\Core\Site\Settings;
 use Drush\Commands\DrushCommands;
-use Symfony\Component\HttpFoundation\Request;
 
 /**
- *
- * In addition to a commandfile like this one, you need a drush.services.yml
- * in root of your module.
- *
- * See these files for an example of injecting Drupal services:
- *   - http://cgit.drupalcode.org/devel/tree/src/Commands/DevelCommands.php
- *   - http://cgit.drupalcode.org/devel/tree/drush.services.yml
+ * Command file for setting-get.
  */
 class DrupalSettingsCommands extends DrushCommands {
 
